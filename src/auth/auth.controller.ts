1f20abfeae759727ed6a49b608287f251f2abe68
itsFDavid <FDavid04@icloud.com>
2025-03-06 23:04:19 -0600

    implement Auth module with user registration, login, verification, and guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,12 @@
-import { Controller, Get, Inject, Post } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { Body, Controller, Get, Inject, Post, Req, UseGuards } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { SERVICES } from 'src/config';
+import { LoginUserDto, RegisterUserDto } from './dto';
+import { catchError } from 'rxjs';
+import { Request } from 'express';
+import { AuthGuard } from './guards/auth.guard';
+import { User, Token } from './decorators';
+import { CurrentUser } from './interfaces/current-user.interface';
 
 
 @Controller('auth')
@@ -10,17 +16,30 @@ export class AuthController {
   ) {}
 
   @Post('register')
-  registerUser() {
-    return this.client.send('auth.register.user', {});
+  registerUser(@Body() registerUserDto: RegisterUserDto) {
+    return this.client.send('auth.register.user', registerUserDto).pipe(
+      catchError( (error) => {
+        throw new RpcException(error);
+      })
+    );
   }
 
   @Post('login')
-  loginUser() {
-    return this.client.send('auth.login.user', {});
+  loginUser(@Body() loginUserDto: LoginUserDto ) {
+    return this.client.send('auth.login.user', loginUserDto).pipe(
+      catchError( (error) => {
+        throw new RpcException(error);
+      })
+    );
   }
 
+  @UseGuards( AuthGuard )
   @Get('verify')
-  verifyUser() {
-    return this.client.send('auth.verify.user', {});
+  verifyUser(
+    @User() user: CurrentUser,
+    @Token() token: string,
+  ) {
+
+    return { user, token };
   }
 }
